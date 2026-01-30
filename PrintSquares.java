847ed28a3b4ad4986a5fa07c0f5c8330e89067c0
kkb204 <kkb204@exeter.ac.uk>
2026-01-30 12:15:41 +0000

    PrintSquares completed with for loop

---
+++ PrintSquares.java
@@ -0,0 +1,19 @@
+import java.util.Scanner; // We use the Scanner class from java.util to handle user input
+
+public class PrintSquares {
+    public static void main(String[] args) {
+
+        // Declare Scanner object
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Please enter the start value: ");
+        int startNum = in.nextInt(); // Reads in an int from standard input
+        
+        System.out.print("Please enter the end value: ");
+        int endNum = in.nextInt();
+
+        for (int i = startNum; i <= endNum; ++i) {
+            System.out.print(i*i + ", ");
+        }
+    }
+}
